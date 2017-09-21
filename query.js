03371cc465b41bb72473dbdb40c594ea87d41a6f
Lawrence Lau <ktslwy@gmail.com>
2017-09-21 11:55:17 -0700

    Create query.js

---
+++ query.js
@@ -0,0 +1,55 @@
+(function() {
+    try {
+        var namespace = 'query';
+
+        function parseQuery(qs) {
+            var obj = {};
+
+            if (typeof qs !== 'string' || qs.length === 0) {
+                return obj;
+            }
+
+            var regexp = /\+/g;
+            qs = qs.split('&');
+
+            var len = qs.length;
+            var x, idx, kstr, vstr, k, v;
+
+            for (var i = 0; i < len; i++) {
+                x = qs[i].replace(regexp, '%20');
+                idx = x.indexOf('=');
+
+                if (idx >= 0) {
+                    kstr = x.substr(0, idx);
+                    vstr = x.substr(idx + 1);
+                } else {
+                    kstr = x;
+                    vstr = '';
+                }
+
+                k = decodeURIComponent(kstr);
+                v = decodeURIComponent(vstr);
+
+                if (!obj.hasOwnProperty(k)) {
+                    obj[k] = v;
+                } else if (Array.isArray(obj[k])) {
+                    obj[k].push(v);
+                } else {
+                    obj[k] = [obj[k], v];
+                }
+            }
+
+            return obj;
+        }
+
+        window.diver.processors[namespace] = {
+            namespace: namespace,
+            process: function process(traffic) {
+                var url = new URL(traffic.request.url);
+                var query = url.search.substr(1);
+
+                return parseQuery(query);
+            }
+        };
+    } catch (e) {}
+})();
