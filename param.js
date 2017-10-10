08b4f22625e32fb6ceb28644ab2eb10b881b819c
Lawrence Lau <ktslwy@gmail.com>
2017-10-09 22:21:32 -0700

    Create param.js

---
+++ param.js
@@ -0,0 +1,48 @@
+(function() {
+    try {
+        var namespace = 'query';
+
+        window.diver.processors[namespace] = {
+            name: 'Query',
+            namespace: namespace,
+            process: function process(traffic) {
+                const obj = {};
+
+                try {
+                    const startIndex = traffic.request.url.indexOf(';');
+                    let params;
+
+                    if (startIndex === -1) {
+                        return obj;
+                    }
+
+                    traffic.request.url.substr(startIndex + 1).split(';').forEach((param) => {
+                        const index = param.indexOf('=');
+                        let kstr, vstr, k, v;
+
+                        if (index >= 0) {
+                            kstr = param.substr(0, index);
+                            vstr = param.substr(index + 1);
+                        } else {
+                            kstr = param;
+                            vstr = '';
+                        }
+
+                        k = decodeURIComponent(kstr);
+                        v = decodeURIComponent(vstr);
+
+                        if (!obj.hasOwnProperty(k)) {
+                            obj[k] = v;
+                        } else if (Array.isArray(obj[k])) {
+                            obj[k].push(v);
+                        } else {
+                            obj[k] = [obj[k], v];
+                        }
+                    });
+                } catch (e) {}
+
+                return obj;
+            }
+        };
+    } catch (e) {}
+})();
