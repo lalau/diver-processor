dc80191250f2f72d6f310d59d6579017fe6991d4
Lawrence Lau <ktslwy@gmail.com>
2017-10-13 16:50:13 -0700

    Create mixpanel.js

---
+++ mixpanel.js
@@ -0,0 +1,34 @@
+(function() {
+    try {
+        var namespace = 'mixpanel';
+
+        window.diver.processors[namespace] = {
+            name: 'Mixpanel',
+            namespace: namespace,
+            process: function process(traffic) {
+                var params;
+                var obj;
+
+                traffic.request.queryString.some(({name, value}) => {
+                    if (name === 'params') {
+                        params = value;
+                    }
+                });
+
+                if (!params) {
+                    return {};
+                }
+
+                try {
+                    obj = JSON.parse(atob(params.replace('data=', '')));
+                } catch (e) {
+                    obj = {};
+                }
+
+                obj.properties.event = obj.event;
+
+                return obj.properties;
+            }
+        };
+    } catch (e) {}
+})();
